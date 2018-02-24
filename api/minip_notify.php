6dd8dc5e8ade99b9c6829d9adb188c4b2c82e5ff
shadowwing@163.com <shadowwing@163.com>
2018-02-24 10:04:50 +0800

    clear unuseless

--- api/minip_notify.php
+++
@@ -1,7 +0,0 @@
-<?php
-include_once 'conf.php';
-include_once '../library/inc.php';
-include '../library/cls.wepay.php';
-
-$wepay = new Wpay();
-$wepay->notify_url();
--- wxwork/config/notice.php
+++
@@ -1,4 +0,0 @@
-<?php
-define('SUITEID', 'wx1741a230da884c96');
-define('SUITESCRET', 'gThtSJ0VyZi08SlxgNvDtM0hSwQA0kIMml_oMYTcyPo');
-define('TITLE', '通知');
